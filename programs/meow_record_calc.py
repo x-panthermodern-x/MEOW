c7c2501ffd164ee211da5d75057b131cdbe2dc27
x-panthermodern-x <pm.la.human@gmail.com>
2023-04-14 23:36:36 -0700

    added est tax to cost in meow_record_calc

--- programs/meow_record_calc.py
+++ programs/meow_record_calc.py
@@ -7,9 +7,37 @@ GREEN = "\033[32m"
 CYAN = "\033[36m"
 MAG = "\033[35m"
 RESET = "\033[0m"
+
+# California tax brackets for 2021 (simplified)
+tax_brackets = [
+    (0, 9850, 0.01),
+    (9851, 20250, 0.02),
+    (20251, 40500, 0.04),
+    (40501, 67500, 0.06),
+    (67501, 405000, 0.08),
+    (405001, 675000, 0.093),
+    (675001, 1000000, 0.103),
+    (1000001, 5000000, 0.113),
+    (5000001, float('inf'), 0.123)
+]
+
 print("-----------------------------------")
 print(f"{RED} MEOW RECORD PRODUCTION CALCULATOR{RESET}")
 print("-----------------------------------\n")
+
+def calculate_income_tax(net_profit, tax_brackets):
+    tax = 0
+    remaining_profit = net_profit
+
+    for low, high, rate in tax_brackets:
+        if remaining_profit > low:
+            bracket_profit = min(remaining_profit - low, high - low)
+            tax += bracket_profit * rate
+            remaining_profit -= bracket_profit
+        else:
+            break
+
+    return tax
 
 def record_calculator(records, sell_price, print_cost, mail_order_wage, records_per_day_range, packaging_cost, mail_order_assistants, shipping_per_unit, weight_per_record):
     # Calculate total revenue
@@ -25,7 +53,7 @@ def record_calculator(records, sell_price, print_cost, mail_order_wage, records_
     total_records_per_day = sum(records_per_assistant)
     days_needed = math.ceil(records / sum(records_per_assistant))
 
-     # Calculate mail order people's total wage
+    # Calculate mail order people's total wage
     total_hours_needed = math.ceil(records / total_records_per_day) * 8
     mail_order_total_wage = total_hours_needed * mail_order_wage * mail_order_assistants
 
@@ -33,40 +61,34 @@ def record_calculator(records, sell_price, print_cost, mail_order_wage, records_
     total_packaging_cost = records * packaging_cost
 
     # Calculate shipping costs
-    
     shipping_cost = records * shipping_per_unit
-
+     
+     # Calculate income tax
+    income_tax = calculate_income_tax(total_revenue, tax_brackets)
 
     # Calculate total cost
-    total_cost = total_print_cost + total_packaging_cost + mail_order_total_wage + shipping_cost
+    total_cost = total_print_cost + total_packaging_cost + mail_order_total_wage + shipping_cost + income_tax
 
     # Calculate total profit
     total_profit = total_revenue - total_cost
 
-    return total_revenue, total_print_cost, total_records_per_day, days_needed, mail_order_total_wage, total_packaging_cost, shipping_cost, total_cost, total_profit, records_per_assistant
+    return total_revenue, total_print_cost, total_records_per_day, days_needed, mail_order_total_wage, total_packaging_cost, shipping_cost, total_cost, income_tax, total_profit, records_per_assistant
 
 # Variables
 records = 2000 #Total Amount of record your printing
-sell_price = 25 #Price you are selling your records at
+sell_price = 35 #Price you are selling your records at
 unit_cost = 6 #Average cost per vinyl unit
-mail_order_assistants = 2 #How many people you are hiring to help you out
+mail_order_assistants = 4 #How many people you are hiring to help you out
 mail_order_wage = 20 #How much you are paying your assistants
 records_per_day_range = (100, 160)  # Range for records packed per day by each mail assistant, we are accounting for the slackers here
 packaging_cost = 3  # Assuming $3 per record for packaging cost to ship to fans
 shipping_per_unit = 0.69 #based off atoz media this is $0.69
-
-#NOT USED / it was not as accurate but can be used for additional calculations
-shipping_rate = 0.05  # $0.05 per kg per 1,000 km
-distance = 4488  # Distance from Italy to Los Angeles in km
-weight_per_record = 0.2 #Kilograms
-total_weight = records * weight_per_record
-shipping_weight_cost = total_weight * shipping_rate * (distance / 1000)
-
+weight_per_record = 0.2 # Kilograms
 
 var_dict = {'Amount of Records: ': records, 'Price you are selling records at: $': sell_price, 'Record Unit Cost: $': unit_cost, 'Mail Order Assistant $/hr: $': mail_order_wage, "Packaging Material Cost: $": packaging_cost, "Number of Mail Order Assistants: ": mail_order_assistants}
 
 # Calculate and print totals
-total_revenue, total_print_cost, total_records_per_day, days_needed, mail_order_total_wage, total_packaging_cost, shipping_cost, total_cost, total_profit, records_per_assistant = record_calculator(records, sell_price, unit_cost, mail_order_wage, records_per_day_range, packaging_cost, mail_order_assistants, shipping_per_unit, weight_per_record)
+total_revenue, total_print_cost, total_records_per_day, days_needed, mail_order_total_wage, total_packaging_cost, shipping_cost, total_cost, income_tax, total_profit, records_per_assistant = record_calculator(records, sell_price, unit_cost, mail_order_wage, records_per_day_range, packaging_cost, mail_order_assistants, shipping_per_unit, weight_per_record)
 
 for var_name, var_value in var_dict.items():
     print(f"{CYAN}{var_name}{var_value}{RESET}")
@@ -80,6 +102,7 @@ print(f"{RED}Total Unit Print cost: ${total_print_cost}")
 print(f"Total cost to ship from pressing plant: ${shipping_cost}")
 print(f"Mail order assistant total wage: ${mail_order_total_wage}")
 print(f"Total packaging cost: ${total_packaging_cost}")
+print(f"Est Tax on GROSS: ${income_tax}")
 print(f"Total cost: ${total_cost}{RESET}")
 
 print(f"{GREEN}Total NET profit: ${total_profit}{RESET}")
@@ -89,7 +112,7 @@ total_costs = []
 total_profits = []
 
 for _ in range(num_runs):
-    _, _, _, _, _, _, _, total_cost, total_profit, _ = record_calculator(records, sell_price, unit_cost, mail_order_wage, records_per_day_range, packaging_cost, mail_order_assistants, shipping_per_unit, weight_per_record)
+    _, _, _, _, _, _, _, total_cost, _, total_profit, _ = record_calculator(records, sell_price, unit_cost, mail_order_wage, records_per_day_range, packaging_cost, mail_order_assistants, shipping_per_unit, weight_per_record)
     total_costs.append(total_cost)
     total_profits.append(total_profit)
 
@@ -99,3 +122,4 @@ average_total_profit = sum(total_profits) / num_runs
 
 print(f"\n{CYAN}Average Total Cost after {num_runs} runs: ${average_total_cost:.2f}")
 print(f"Average Total Net Profit after {num_runs} runs: ${average_total_profit:.2f}{RESET}")
+
